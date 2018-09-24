34f7c1ad97f34bc3bcbd18b7f6a26f3af88237ec
Vincent Schuurhof <vincento2009@hotmail.com>
2018-09-24 13:19:49 +0200

    C#
    LOGBOG STUFF GG

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            opretKunde();
+        }
+
+        private static void opretKunde()
+        {
+            string statement = "insert into kunder values ('" + "Anders And" + "','" + "Telegrafvej 9" + "'," + 90 + ")";
+
+            SqlTest.insert(statement);
+        }
+    }
+}
