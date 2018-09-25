9772e110c6311bac29081a471c9e59eb0aea3feb
Vincent Schuurhof <vincento2009@hotmail.com>
2018-09-25 11:47:59 +0200

    Updated

---
+++ Visual studio project/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            opretKunde();
+            SqlTest.Select("select * from kunder");
+            Console.ReadKey();
+        }
+
+        private static void opretKunde()
+        {
+            string statement = "insert into kunder values ('" + "Jesper" + "','" + "Fårekylling" + "'," + "2800" + ")";
+
+            SqlTest.insert(statement);
+        }
+    }
+}
---
+++ Visual studio project/SqlTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQL_Connection
+{
+    class SqlTest
+    {
+        private static string ConnectionString = "Data Source = SKAB2-PC-03;Initial Catalog=Autoværksted; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static void insert(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static void Select(string sql)
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+                adapter.Fill(table);
+
+                foreach (DataRow kunde in table.Rows)
+                {
+                    Console.WriteLine(kunde["kundeid"].ToString());
+                    Console.WriteLine(kunde["fornavn"].ToString());
+                    Console.WriteLine(kunde["efternavn"].ToString());
+                    Console.WriteLine(kunde["postnr"].ToString());
+                    Console.WriteLine();
+                }
+
+                // denførsterække = table.Rows[0] ["navn"].ToString();            
+            }
+        }
+    }
+
+}
