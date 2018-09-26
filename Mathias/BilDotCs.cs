63485b1935bc77c607c0236b5c68a8699bd22e1c
Mathias617 <43535163+Mathias617@users.noreply.github.com>
2018-09-26 10:56:56 +0200

    Add files via upload

---
+++ Mathias/BilDotCs.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQL_Connection
+{
+    class BilDotCs
+    {
+        private static string ConnectionString = "Data Source = SKAB2-PC-09;Initial Catalog=Autoværksted; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static void insert(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static void Select(string sql)
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+                adapter.Fill(table);
+
+                foreach (DataRow bil in table.Rows)
+                {
+                    Console.WriteLine(bil["id"].ToString());
+                    Console.WriteLine(bil["mærke"].ToString());
+                    Console.WriteLine(bil["model"].ToString());
+                    Console.WriteLine(bil["årgang"].ToString());
+                    Console.WriteLine(bil["km"].ToString());
+                    Console.WriteLine(bil["brændstoftype"].ToString());
+                    Console.WriteLine(bil["vægt"].ToString());
+                }
+
+                // denførsterække = table.Rows[0] ["navn"].ToString();            
+            }
+        }
+        public static void update(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static void delete(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
---
+++ Mathias/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            opretKunde();
+            SqlTest.Select("select * from kunder");
+            opretBil();
+            BilDotCs.Select("select * from bil");
+            updateBil();
+            deleteKunde();
+            Console.ReadKey();
+
+        }
+
+        private static void opretKunde()
+        {
+            string statement = "insert into kunder values ('" + "Jesper" + "','" + "Fårekylling" + "'," + "2800" + ")";
+
+            SqlTest.insert(statement);
+        }
+
+
+        private static void opretBil()
+        {
+            string statement = "insert into bil values (" + "1" + ",'" + "Honda" + "','" + "Civic" + "'," + "2008" + "," + "200000" + ",'" + "Diesel" + "'," + "1050" + ")";
+
+            BilDotCs.insert(statement);
+
+        }
+        private static void updateBil()
+        {
+            string statement = "update kunder set fornavn = 'Mads' where id = 1";
+            BilDotCs.update(statement);
+        }
+        private static void deleteKunde()
+        {
+            string statement = "delete from bil where id = 1";
+            string statement2 = "delete from kunder where id = 1";
+       
+            SqlTest.delete(statement);
+            SqlTest.delete(statement2);
+        }
+
+    }
+}
